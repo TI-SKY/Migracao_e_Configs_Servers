1515fe4aa0f697e7af1ae1cf047dff14802cdcd5
sasouzacsp <83767569+sasouzacsp@users.noreply.github.com>
2022-06-07 11:32:37 -0300

    Update script_config_sistemas.bat

--- script_config_sistemas.bat
+++ script_config_sistemas.bat
@@ -8,7 +8,7 @@ reg delete HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkyLivros\BancoDados\Databa
 reg delete HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkySigner\BancoDados\Database /v "TemporaryDir" /f
 reg delete HKEY_CURRENT_USER\Software\SkyInformatica\SeloDigital\BancoDados\Database /v "TemporaryDir" /f
 reg delete HKEY_CURRENT_USER\Software\SkyInformatica\SkyMonitor\BancoDados\Database /v "TemporaryDir" /f
-
+reg delete HKEY_CURRENT_USER\Software\SkyBiometrics\BancoDadosD10\Database /v "TemporaryDir" /f
 
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\Financeiro\BancoDados\Database c:\financeiro.reg
 reg export HKEY_CURRENT_USER\Software\Notar\BancoDadosD7\Database c:\notar.reg
@@ -20,9 +20,10 @@ reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkyLivros\BancoDados\Databa
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkySigner\BancoDados\Database c:\skysigner.reg
 reg export HKEY_CURRENT_USER\Software\SkyInformatica\SeloDigital\BancoDados\Database c:\selo.reg
 reg export HKEY_CURRENT_USER\Software\SkyInformatica\SkyMonitor\BancoDados\Database c:\skywebmonitor.reg
+reg export HKEY_CURRENT_USER\Software\SkyBiometrics\BancoDadosD10\Database c:\skybiometrics.reg
 
 cd\
-copy financeiro.reg+imoveis.reg+protesto.reg+skylivros.reg+skysigner.reg+ted.reg+notar.reg+civil.reg+selo.reg+skywebmonitor.reg sistemas.reg
+copy financeiro.reg+imoveis.reg+protesto.reg+skylivros.reg+skysigner.reg+ted.reg+notar.reg+civil.reg+selo.reg+skywebmonitor.reg+skybiometrics.reg sistemas.reg
 
 exit
 
