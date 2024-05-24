fd29dac40fa6f1ad53ae915ee37a578497d02ab0
Vini-boat <85585157+Vini-boat@users.noreply.github.com>
2024-05-24 16:40:43 -0300

    Update script_config_sistemas.bat
    adicionado chave do livro caixa

--- script_config_sistemas.bat
+++ script_config_sistemas.bat
@@ -1,3 +1,4 @@
+reg delete HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\LivroCaixa\BancoDados\Database /v "TemporaryDir" /f
 reg delete HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\Financeiro\BancoDados\Database /v "TemporaryDir" /f
 reg delete HKEY_CURRENT_USER\SOFTWARE\Notar\BancoDadosD7\Database /v "TemporaryDir" /f
 reg delete HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\Civil\BancoDados\Database /v "TemporaryDir" /f
@@ -10,12 +11,13 @@ reg delete HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SeloDigital\BancoDados\Data
 reg delete HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkyMonitor\BancoDados\Database /v "TemporaryDir" /f
 reg delete HKEY_CURRENT_USER\SOFTWARE\SkyBiometrics\BancoDadosD10\Database /v "TemporaryDir" /f
 
+reg export HKEY_CURRENT_USER\Software\SkyInformatica\LivroCaixa\BancoDados c:\livrocaixa.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\Financeiro\BancoDados c:\financeiro.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\Notar\BancoDadosD7 c:\notar.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\Civil\BancoDados c:\civil.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\TED\BancoDados c:\ted.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\Imoveis\BancoDados c:\imoveis.reg
-reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\Protesto\BancoDados\Database c:\protesto.reg
+reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\Protesto\BancoDados c:\protesto.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkyLivros\BancoDados c:\skylivros.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkySigner\BancoDados c:\skysigner.reg
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SeloDigital\BancoDados c:\selo.reg
@@ -23,7 +25,7 @@ reg export HKEY_CURRENT_USER\SOFTWARE\SkyInformatica\SkyMonitor\BancoDados c:\sk
 reg export HKEY_CURRENT_USER\SOFTWARE\SkyBiometrics\BancoDadosD10 c:\skybiometrics.reg
 
 cd\
-copy financeiro.reg+imoveis.reg+protesto.reg+skylivros.reg+skysigner.reg+ted.reg+notar.reg+civil.reg+selo.reg+skywebmonitor.reg+skybiometrics.reg sistemas.reg
+copy livrocaixa.reg+financeiro.reg+imoveis.reg+protesto.reg+skylivros.reg+skysigner.reg+ted.reg+notar.reg+civil.reg+selo.reg+skywebmonitor.reg+skybiometrics.reg sistemas.reg
 
 exit
 
